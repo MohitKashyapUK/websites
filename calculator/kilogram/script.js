07793542977b71cb4d9316fa6cede59244745482
Mohit <mohitkashyap46707@gmail.com>
2024-04-26 19:20:43 +0530

    dfgsdfgdfg

---
+++ calculator/kilogram/script.js
@@ -0,0 +1,53 @@
+const $qs = selector => document.querySelector(selector);
+
+const container = $qs("main > div");
+const children = container.children;
+
+const first_container = children[0];
+const second_container = children[1];
+const third_container = children[2];
+
+const first_container_inputs = first_container.querySelectorAll('input');
+const second_container_inputs = second_container.querySelectorAll('input');
+const third_container_inputs = third_container.querySelectorAll("input");
+
+const first_output = first_container.querySelector("span");
+const second_output = second_container.querySelector("span");
+const third_output = third_container.querySelector('span');
+
+function first() {
+  const price = Number(first_container_inputs[0].value);
+  const kg = Number(first_container_inputs[1].value);
+  if (!(price && kg)) {
+    first_output.innerText = "KG";
+    return;
+  }
+  first_output.innerText = price / kg;
+}
+
+function second() {
+  const price = Number(second_container_inputs[0].value);
+  const kg = Number(second_container_inputs[1].value);
+  const weight = Number(second_container_inputs[2].value);
+  if (!(price && kg && weight)) {
+    second_output.innerText = "Price";
+    return;
+  }
+  second_output.innerText = (price / kg)*weight;
+}
+
+function third() {
+  const total_price = Number(third_container_inputs[0].value);
+  const kg = Number(third_container_inputs[1].value);
+  const price = Number(third_container_inputs[2].value);
+  if (!(total_price && kg && price)) {
+    third_output.innerText = "";
+    return;
+  }
+  // const kg_price = ;
+  third_output.innerText = price/(total_price/kg);
+}
+
+for (let index = 0; index < first_container_inputs.length; index++) first_container_inputs[index].addEventListener("input", first);
+for (let index = 0; index < second_container_inputs.length; index++) second_container_inputs[index].addEventListener("input", second);
+for (let index = 0; index < third_container_inputs.length; index++) third_container_inputs[index].addEventListener("input", third);
---
+++ calculator/percentage/script.js
@@ -0,0 +1,39 @@
+const $qs = selector => document.querySelector(selector);
+const container = $qs("main > div");
+const children = container.children;
+const first_list = children[0].querySelector(".input").querySelectorAll("input");
+const second_list = children[1].querySelector(".input").querySelectorAll("input");
+const first_output = $qs(".formula-one-output");
+const second_output = $qs(".formula-two-output");
+
+function formula_one() {
+  let part = Number(first_list[0].value);
+  let whole = Number(first_list[1].value);
+  let percent = Number(first_list[2].value);
+  if (!(part && whole && percent)) {
+    first_output.innerText = null;
+    return;
+  }
+  first_output.innerText = (part/whole)*percent;
+}
+
+function formula_two() {
+  let percent = Number(second_list[0].value);
+  let part = Number(second_list[1].value);
+  let whole = Number(second_list[2].value);
+  if (!(part && whole && percent)) {
+    second_output.innerText = "Value";
+    return;
+  }
+  second_output.innerText = (percent/part)*whole;
+}
+
+for (let index = 0; index < second_list.length; index++) {
+  const element = first_list[index];
+  element.addEventListener("input", formula_one);
+}
+
+for (let index = 0; index < second_list.length; index++) {
+  const element = second_list[index];
+  element.addEventListener("input", formula_two);
+}
