f968034bfb517e7ab9da1b07371406482f838b77
Mohit <mohitkashyap46707@gmail.com>
2024-04-27 17:46:58 +0530

    asdfdsf

--- calculator/kilogram/script.js
+++ calculator/kilogram/script.js
@@ -7,47 +7,39 @@ const first_container = children[0];
 const second_container = children[1];
 const third_container = children[2];
 
-const first_container_inputs = first_container.querySelectorAll('input');
-const second_container_inputs = second_container.querySelectorAll('input');
-const third_container_inputs = third_container.querySelectorAll("input");
+const inputs = document.querySelectorAll("input");
 
 const first_output = first_container.querySelector("span");
 const second_output = second_container.querySelector("span");
 const third_output = third_container.querySelector('span');
 
-function first() {
-  const price = Number(first_container_inputs[0].value);
-  const kg = Number(first_container_inputs[1].value);
-  if (!(price && kg)) {
+const price_per_kg = (total_price, weight_in_kg) => parseFloat((total_price/weight_in_kg).toFixed(2));
+const unit_price = (price_per_kg, unit_in_kg) => parseFloat((price_per_kg*unit_in_kg).toFixed(2));
+const kg_by_price = (price_unit, per_kg_price) => parseFloat((price_unit/per_kg_price).toFixed(3));
+
+function calculator() {
+  const total_price = Number(inputs[0].value);
+  const total_weight_in_kg = Number(inputs[1].value);
+  
+  if (!(total_price && total_weight_in_kg)) {
     first_output.innerText = null;
-    return;
-  }
-  first_output.innerText = parseFloat((price / kg).toFixed(2));
-}
-
-function second() {
-  const price = Number(second_container_inputs[0].value);
-  const kg = Number(second_container_inputs[1].value);
-  const weight = Number(second_container_inputs[2].value);
-  if (!(price && kg && weight)) {
     second_output.innerText = null;
+    third_output.innerText = null;
     return;
   }
-  second_output.innerText = parseFloat(((price / kg)*weight).toFixed(2));
+
+  const per_kg_price = price_per_kg(total_price, total_weight_in_kg);
+  first_output.innerText = per_kg_price;
+
+  const unit_in_kg = Number(inputs[2].value);
+  if (unit_in_kg) {
+    second_output.innerText = unit_price(per_kg_price, unit_in_kg);
+  } else second_output.innerText = null;
+
+  const price_unit = Number(inputs[3].value);
+  if (price_unit) {
+    third_output.innerText = kg_by_price(price_unit, per_kg_price);
+  } else third_output.innerText = null;
 }
 
-function third() {
-  const total_price = Number(third_container_inputs[0].value);
-  const kg = Number(third_container_inputs[1].value);
-  const price = Number(third_container_inputs[2].value);
-  if (!(total_price && kg && price)) {
-    third_output.innerText = "";
-    return;
-  }
-  // const kg_price = ;
-  third_output.innerText = parseFloat((price/(total_price/kg)).toFixed(3));
-}
-
-for (let index = 0; index < first_container_inputs.length; index++) first_container_inputs[index].addEventListener("input", first);
-for (let index = 0; index < second_container_inputs.length; index++) second_container_inputs[index].addEventListener("input", second);
-for (let index = 0; index < third_container_inputs.length; index++) third_container_inputs[index].addEventListener("input", third);
+for (let index = 0; index < inputs.length; index++) inputs[index].addEventListener("input", calculator);
